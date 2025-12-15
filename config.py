50bd09096651c22c91b95d2a3e0922ed3332a25d
Alishahryar1 <alishahryar2@gmail.com>
2025-12-15 14:46:08 -0800

    perf: switch cuDNN from deterministic to benchmark mode.

--- config.py
+++ config.py
@@ -148,5 +148,5 @@ def seed_everything(seed=42):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
     print(f"Using device: {CONFIG['device']}")
